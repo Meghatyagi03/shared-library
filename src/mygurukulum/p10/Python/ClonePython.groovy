db92d5393666ed43fb4ae22a9524f9058e0dc2d4
Megha Tyagi <161687882+Meghatyagi03@users.noreply.github.com>
2024-10-13 19:35:01 +0530

    Update ClonePython.groovy

--- src/mygurukulum/p10/Python/ClonePython.groovy
+++ src/mygurukulum/p10/Python/ClonePython.groovy
@@ -1,1 +1,13 @@
+package mygurukulum.p10.Python
 
+class ClonePython {
+    def script
+
+    ClonePython(script) {
+        this.script = script
+    }
+
+    def execute() {
+        script.git(branch: 'main', url: 'https://github.com/mygurukulam-p10/attendence-api.git', credentialsId: 'git-token')
+    }
+}
